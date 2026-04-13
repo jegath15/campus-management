d752a40436e7213dce68f0d2969b686dc138815e
Jegath Pranava <jegathpranava@gmail.com>
2026-04-14 03:30:26 +0530

    Security Update: Implemented project-wide 'Deep Clear' logout to enable seamless portal switching

--- assets/logout.php
+++ assets/logout.php
@@ -1,11 +1,19 @@
 <?php 
-    session_start();
+    include_once('config.php');
 
-    unset($_SESSION);
+    $_SESSION = array(); 
     session_destroy();
+
+    // Reset cookie
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', time() - 42000,
+            $params["path"], $params["domain"],
+            $params["secure"], $params["httponly"]
+        );
+    }
 
     $response = array('status' => 'success',
                      'message' => 'Logout successful');
     echo json_encode($response);
-
 ?>
